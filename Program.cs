2f1a9b4038abe95b8d58ac68620827d739597ee9
MOHAMMAD ANAS <92999181+MohammadAnas15@users.noreply.github.com>
2024-08-07 08:58:44 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿ GetProcessedDataAsync("some text");
+Console.WriteLine("Some other Indepensent Work");
+
+Console.WriteLine("The End...!");
+Console.ReadKey();
+
+async void GetProcessedDataAsync(string data)
+{
+    Console.WriteLine("GetProcessedDataAsync Starts");
+    string processedData = await ProcessDataAsync(data);
+    Console.WriteLine(processedData);
+}
+
+ async Task<string> ProcessDataAsync(string data)
+{
+    // Simulate data processing
+    await Task.Delay(5000);
+    Console.WriteLine("ProcessDataAsync End");
+    return data.ToUpper();
+}
+
+
